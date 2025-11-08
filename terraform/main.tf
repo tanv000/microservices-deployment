f04af4621cb0f7bde0b4bbd4ddff514740333c69
Settem Tanvi <144032915+tanv000@users.noreply.github.com>
2025-11-08 16:11:14 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,7 +95,6 @@ data "aws_ami" "amazon_linux_2" {
 resource "aws_instance" "app_instance" {
   ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = var.instance_type
-  instance_state         = "running"
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
